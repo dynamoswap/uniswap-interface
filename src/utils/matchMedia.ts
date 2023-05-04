1639e1d75688cc5a37c3c363debbf36487c6aa41
Vignesh Mohankumar <me@vig.xyz>
2023-05-04 19:21:11 -0400

    ignore

--- src/utils/matchMedia.ts
+++ src/utils/matchMedia.ts
@@ -3,16 +3,16 @@
 
 export function addMediaQueryListener(mediaQuery: MediaQueryList, listener: (event: MediaQueryListEvent) => void) {
   try {
-    mediaQuery.addEventListener('change', listener)
+    mediaQuery.addEventListener('change', listener) // eslint-disable-line no-restricted-syntax
   } catch (e) {
-    mediaQuery.addListener(listener)
+    mediaQuery.addListener(listener) // eslint-disable-line no-restricted-syntax
   }
 }
 
 export function removeMediaQueryListener(mediaQuery: MediaQueryList, listener: (event: MediaQueryListEvent) => void) {
   try {
-    mediaQuery.removeEventListener('change', listener)
+    mediaQuery.removeEventListener('change', listener) // eslint-disable-line no-restricted-syntax
   } catch (e) {
-    mediaQuery.removeListener(listener)
+    mediaQuery.removeListener(listener) // eslint-disable-line no-restricted-syntax
   }
 }
